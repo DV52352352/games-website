2cfa46664669cbf94ad979bf11d2bd4f8b9b3eaa
Dhrutik Vala <dhrutik.vala@gmail.com>
2024-04-22 15:03:29 +0100

    Making progress in the games page by adding a description

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,13 @@ menuToggler.addEventListener('click', ev => {
     menu.classList.toggle('open');
     menuToggler.textContent = menuToggler.textContent === "×" ? "≡" : "×";
   });
+
+let pageSize = 12;
+let currentPage;
+let objectIDs;
+  
+async function loadObject(id) {
+  const url = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`;
+  const response = await fetch(url);
+  return response.json();
+}
